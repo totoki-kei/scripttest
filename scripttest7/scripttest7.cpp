139199830d28f5da22914e807b5e85bf702e7557
totoki_kei <totoki_kei@outlook.com>
2024-05-05 15:17:43 +0900

    nya-n

--- scripttest7/scripttest7.cpp
+++ scripttest7/scripttest7.cpp
@@ -1,258 +1,175 @@
 ﻿// scripttest7.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
 //
 
+#include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <regex>
 #include <unordered_map>
 #include <variant>
 #include <vector>
+#include <string>
+
+#include "string_manipulation.h"
 
 using namespace std::string_literals;
 using namespace std::string_view_literals;
 
-template <typename Iter>
-std::vector<std::string> split_by_line(Iter const whole_begin, Iter const whole_end) {
-	std::vector<std::string> ret;
+struct SourceLine;
+struct Entity;
 
-	std::string line;
-
-	for (auto it = whole_begin; it != whole_end; ++it) {
-		if (*it == '\n') {
-			// 末尾のコントロール空白文字(改行2種)を削除
-			while (std::ispunct(line.back()) && std::isspace(line.back())) line.pop_back();
-			ret.push_back(line);
-
-			line.clear();
-		}
-		else {
-			line.push_back(*it);
-		}
-	}
-
-	if (!line.empty()) {
-		// 末尾のコントロール空白文字(改行2種)を削除
-		while (std::ispunct(line.back()) && std::isspace(line.back())) line.pop_back();
-		ret.push_back(line);
-
-	}
-
-	return ret;
-}
-
-
-class operation {
-public:
-	std::string op;
-
+struct SourceLine {
+	size_t line_number;
+	std::string text;
 };
 
-struct data_variant;
-using variant_list = std::vector<data_variant>;
-using variant_dictionary = std::unordered_map<std::string, data_variant>;
+struct Label {
+	std::string name;
+	size_t index;
+};
 
-using variant_list_ptr = std::shared_ptr<variant_list>;
-using variant_dictionary_ptr = std::shared_ptr<variant_dictionary>;
+struct Segment {
+	std::string name;
+	std::vector<std::shared_ptr<Entity>> entities;
+	std::vector<Label> labels;
+};
 
-using data_variant_base = std::variant<nullptr_t, int, double, std::string, variant_list_ptr, variant_dictionary_ptr>;
+struct OpcodeDesc {
+	const char* name;
+	int input_count;
+};
 
-struct data_variant : data_variant_base {
-	// inherit base type constructor
-	using data_variant_base::data_variant_base;
-	// inherit base type assignment operator
-	using data_variant_base::operator=;
+OpcodeDesc descset[] = {
+	{ "ret", 1 },
+	{"data", 1 },
+	{"call", 3 },
+};
 
-	data_variant(const data_variant& other) = default;
-	data_variant(data_variant&& other) noexcept : data_variant_base(std::move(other)) {}
+enum class SegmentType : int32_t {
+	// 無効 / (間接参照)間接参照しない
+	Invalid = -1,
+	// 各スクリプトインスタンス内データ領域
+	Local = 0,
+	// スクリプト共通データ領域(静的データ領域)
+	Script = 1,
+	// 実行スタック
+	Stack = 2,
+	// 外部
+	Extern = 3,
+};
 
-	~data_variant() = default;
+struct OperandDesc {
+	SegmentType segment;
+	SegmentType addressing_segment;
+	int32_t offset;
+	std::string label;
+};
 
-	data_variant& operator=(const data_variant& other) {
-		if (this == &other)
-			return *this;
-		data_variant_base::operator =(other);
-		return *this;
+struct EntityDetail {
+	virtual ~EntityDetail() = 0 {}
+};
+
+struct OperationEntityDetail : EntityDetail {
+	OpcodeDesc opcode;
+	OperandDesc destination;
+	OperandDesc operand[ 3 ];
+};
+
+struct AnnotationEntityDetail : EntityDetail {
+	std::string name;
+	std::vector<std::string> flags;
+	std::vector<std::tuple<std::string, std::string>> params;
+};
+
+struct Entity {
+	const SourceLine* line_info;
+	std::unique_ptr<EntityDetail> detail;
+
+	Entity(const SourceLine* line_info)
+		: line_info(line_info) {
 	}
-
-	data_variant& operator=(data_variant&& other) noexcept {
-		if (this == &other)
-			return *this;
-		data_variant_base::operator =(std::move(other));
-		return *this;
+	Entity(const SourceLine* line_info, std::unique_ptr<EntityDetail>&& detail)
+		: line_info(line_info), detail(std::move(detail)) {
 	}
-
 };
 
 
+std::shared_ptr<Entity> make_entity(const SourceLine* source_line, std::vector<std::string>& tokens) {
+	auto detail = std::make_unique<OperationEntityDetail>();
 
-class execution_status {
-public:
-	variant_list variables;
-	std::unordered_map<std::string, size_t> variable_name_table;
 
-	variant_list data_stack;
 
-};
-
-class code_cache {
-
-};
-
-void excecute_line(const std::string& line, execution_status& stat, code_cache* out_code_cache) {
-
+	return std::make_shared<Entity>(source_line, std::move(detail));
 }
 
-void excecute_line(const code_cache& code_cache, execution_status& stat) {
-
-}
-
-
-variant_list_ptr make_variant_list() {
-	return std::make_shared<variant_list>();
-}
-variant_list_ptr make_variant_list(std::initializer_list<variant_list::value_type>&& list) {
-	return std::make_shared<variant_list>(list);
-}
-variant_list_ptr make_variant_list(const variant_list_ptr& list) {
-	return list ? std::make_shared<variant_list>(*list) : std::make_shared<variant_list>();
-}
-variant_list_ptr make_variant_list(variant_list_ptr&& list) {
-	return std::move(list);
-}
-variant_list_ptr make_variant_list(const variant_list& list) {
-	return std::make_shared<variant_list>(list);
-}
-variant_list_ptr make_variant_list(variant_list* list, const bool copy = false) {
-	return copy && list ? std::make_shared<variant_list>(*list) : std::shared_ptr<variant_list>(list);
-}
-
-
-variant_dictionary_ptr make_variant_dictionary() {
-	return std::make_shared<variant_dictionary>();
-}
-variant_dictionary_ptr make_variant_dictionary(std::initializer_list<variant_dictionary::value_type>&& list) {
-	return std::make_shared<variant_dictionary>(list);
-}
-variant_dictionary_ptr make_variant_dictionary(const variant_dictionary_ptr& list) {
-	return list ? std::make_shared<variant_dictionary>(*list) : std::make_shared<variant_dictionary>();
-}
-variant_dictionary_ptr make_variant_dictionary(variant_dictionary_ptr&& list) {
-	return std::move(list);
-}
-variant_dictionary_ptr make_variant_dictionary(const variant_dictionary& list) {
-	return std::make_shared<variant_dictionary>(list);
-}
-variant_dictionary_ptr make_variant_dictionary(variant_dictionary* list, const bool copy = false) {
-	return copy && list ? std::make_shared<variant_dictionary>(*list) : std::shared_ptr<variant_dictionary>(list);
-}
-
-
-constexpr size_t data_variant_size = sizeof(data_variant);
-constexpr size_t data_variant_base_size = sizeof(data_variant_base);
 
 
 int main() {
 
-	class variant_printer {
-		size_t indent_num_;
-		std::string indent_;
-		std::string prefix_;
-		std::string suffix_;
-	public:
-		explicit variant_printer(size_t indent, std::string_view prefix, std::string_view suffix) : indent_num_(indent), indent_(indent, ' '), prefix_(prefix), suffix_(suffix) {}
 
-		void operator ()(nullptr_t) const {
-			std::cout << indent_ << prefix_ << "(nullptr)" << suffix_ << std::endl;
-		}
-
-		void operator()(const int i) const {
-			std::cout << indent_ << prefix_ << i << suffix_ << std::endl;;
-		}
-
-		void operator()(const double n) const {
-			std::cout << indent_ << prefix_ << std::fixed << std::setprecision(3) << n << suffix_ << std::endl;;
-		}
-
-		void operator()(const std::string& s) const {
-			std::cout << indent_ << prefix_ << "'" << s << "'" << suffix_ << std::endl;;
-		}
-
-		void operator ()(const variant_list_ptr& pl) const {
-			if (pl) {
-				if (const auto& v = *pl; v.empty())
-				{
-					std::cout << indent_ << prefix_ << "[]" << suffix_ << std::endl;
-				}
-				else {
-
-					std::cout << indent_ << prefix_ << "[" << std::endl;
-					for (const auto& e : v) {
-						std::visit(variant_printer(indent_num_ + 1, "", ","), e);
-					}
-
-					std::cout << indent_ << "]" << suffix_ << std::endl;
-				}
-			} else
-			{
-				std::cout << indent_ << prefix_ << "[ (empty list) ]" << suffix_ << std::endl;;
-			}
-		}
-
-		void operator() (const variant_dictionary_ptr& pd) const {
-			if (pd) {
-				if (const auto& d = *pd; d.empty())
-				{
-					std::cout << indent_ << prefix_ << "{}" << suffix_ << std::endl;
-				}
-				else {
-					std::cout << indent_ << prefix_ << "{" << std::endl;
-					for (const auto& [key, val] : d) {
-						std::visit(variant_printer(indent_num_ + 1, "'" + key + "': ", ","), val);
-					}
-					std::cout << indent_ << "}" << suffix_ << std::endl;
-				}
-			} else
-			{
-				std::cout << indent_ << prefix_ << "{ (empty dictionary) }" << suffix_ << std::endl;;
-			}
-		}
-	};
-
-	std::cout << sizeof(data_variant) << std::endl;
-
-	data_variant vs = make_variant_list({
-		{ nullptr },
-		{ 10 },
-		{ 2.1 },
-		{ "hoge"s },
-		make_variant_list({
-			{11}, {22}, {"hogehoge"}
-		}),
-		make_variant_dictionary({
-			{ "first", 10 },
-			{ "second", "0x02"s },
-			{ "third", make_variant_list({"this", "is", "a", "pen"})}
-		}),
-		"\n",
-		make_variant_list(),
-		make_variant_list(nullptr),
-		"\n",
-		make_variant_dictionary(),
-		make_variant_dictionary(nullptr),
-	});
-
-	std::visit(variant_printer{0, "", ""}, vs);
-
-#if 0
 	std::fstream fs;
-	fs.open(".\\scripttest7.cpp");
+	fs.open(".\\c.txt");
 
 	auto lines = split_by_line(std::istreambuf_iterator(fs), std::istreambuf_iterator<char>());
-
-	for (auto& line : lines) {
-		std::cout << "<<<" << line << ">>>" << std::endl;
+	std::vector<SourceLine> source_lines;
+	for (size_t i = 0; i < lines.size(); ++i) {
+		source_lines.emplace_back(i, std::string(lines[ i ]));
 	}
-#endif
+	lines.clear();
 
+	std::unordered_map<std::string, Segment> segments;
+	std::vector<Entity> annotations;
+	Segment* current_segment = nullptr;
+	std::string last_label_name;
+	bool in_annotation_block = false;
+
+	for (const auto& line : source_lines) {
+		auto token_list = tokenize(line.text.begin(), line.text.end());
+		for (auto& token : token_list) {
+			std::cout << "<" << token << "> ";
+		}
+		std::cout << std::endl;
+
+		continue;
+
+		if (token_list.empty()) continue;
+
+		if (token_list[ 0 ].starts_with("###")) {
+			// アノテーションブロックの開始/終了
+			in_annotation_block = !in_annotation_block;
+		}
+		else if (in_annotation_block || token_list[ 0 ].starts_with('#')) {
+			// アノテーション行、またはアノテーションブロック内の行
+			annotations.emplace_back(Entity{ &line });
+		}
+		else if (token_list[ 0 ].starts_with('.')) {
+			// セグメント指定
+			if (auto found = segments.find(token_list[ 0 ]); found == segments.end()) {
+				segments.insert(
+					std::make_pair(
+						token_list[ 0 ],
+						Segment{ token_list[ 0 ], {}, {} }
+				));
+			}
+			current_segment = &segments[ token_list[ 0 ] ];
+		}
+		else if (token_list[ 0 ].ends_with(':')) {
+			// ラベル
+			last_label_name = token_list[ 0 ];
+		}
+		else {
+			// エンティティ行
+			if (current_segment == nullptr) {
+				throw std::domain_error("no segment specified");
+			}
+
+			std::shared_ptr<Entity> ent = make_entity(&line, token_list);
+			if (!ent) {
+				throw std::domain_error("no segment specified");
+			}
+			
+		}
+	}
 
 }
 
---
+++ scripttest7/string_manipulation.h
@@ -0,0 +1,101 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <optional>
+
+/**
+ * \brief 入力文字列を行単位で分割 空行含む
+ * \tparam CharIter Iterator
+ * \param whole_begin begin iterator
+ * \param whole_end end iterator
+ * \return list for line
+ */
+template <typename CharIter>
+auto split_by_line(CharIter const whole_begin, CharIter const whole_end) -> std::vector<std::string> {
+	std::vector<std::string> ret;
+
+	std::string line;
+
+	for (auto it = whole_begin; it != whole_end; ++it) {
+		if (*it == '\n') {
+			// 末尾のコントロール空白文字(改行2種)を削除
+			while (!line.empty() && std::ispunct(line.back()) && std::isspace(line.back())) line.pop_back();
+			ret.push_back(line);
+
+			line.clear();
+		}
+		else {
+			line.push_back(*it);
+		}
+	}
+
+	if (!line.empty()) {
+		// 末尾のコントロール空白文字(改行2種)を削除
+		while (std::ispunct(line.back()) && std::isspace(line.back())) line.pop_back();
+		ret.push_back(line);
+
+	}
+
+	return ret;
+}
+
+template <typename CharIter>
+std::vector<std::string> tokenize(CharIter const first, CharIter const last) {
+	std::vector<std::string> ret;
+	std::string work;
+
+	char quote = '\0';
+	bool in_escape = false;
+
+	auto consume =
+		[&](std::string& workarea, const bool allow_empty = false) {
+			if (allow_empty || !workarea.empty()) { ret.push_back(workarea); workarea.clear(); }
+		};
+
+	for (auto it = first; it != last || *it != ';'; /* nop */) {
+		
+		if (in_escape) {
+			// エスケープ中(最優先)
+			work.push_back(*it++);
+			in_escape = false;
+		}
+		else {
+			if (quote == '\0') {
+				// 文字列の外
+				if (std::isspace(*it)) {
+					// 空白文字はトークン区切りとする
+					consume(work);
+					++it;
+				}
+				else if (*it == '\'' || *it == '"') {
+					// 文字列開始
+					consume(work);
+					work.push_back(quote = *it++);
+				}
+				else {
+					work.push_back(*it++);
+				}
+			}
+			else {
+				// 文字列の中
+				if (*it == '\\') {
+					work.push_back(*it++);
+					in_escape = true;
+				}
+				else {
+					char ch;
+					work.push_back(ch = *it++);
+					if (ch == quote) {
+						consume(work);
+					}
+				}
+			}
+		}
+
+	}
+
+	consume(work);
+
+	return ret;
+}
+
